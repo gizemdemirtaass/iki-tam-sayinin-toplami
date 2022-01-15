5110f7507c22148ff171bbf0b17946d474316b46
gizemdemirtaass <79507052+gizemdemirtaass@users.noreply.github.com>
2022-01-15 15:22:08 +0300

    Add files via upload

---
+++ IkiTamSayınınToplamı/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int sayi1 = 10;
+    int sayi2=20;
+    System.out.println("Sayılar: " +sayi1+ " ve " +sayi2);
+    int toplam = sayi1+sayi2;
+    System.out.println("İki tam sayının toplamı: " +toplam);
+
+    }
+}
